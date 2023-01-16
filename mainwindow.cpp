68ab51623680bd1aca7acbd1fddb94b0bf6a2ddb
TQGiser <540484369@qq.com>
2023-01-16 21:16:35 +0800

    aa

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -55,11 +55,11 @@ void MainWindow::on_pushButton_clicked()
 
         char** papszMetadata = poDataset->GetMetadata(NULL);
         /*获取全部元数据信息*/
-//        if(CSLCount(papszMetadata)>0)
-//        {
-//            for(int i=0;papszMetadata[i]!=NULL;i++)
-//                ui->plainTextEdit->appendPlainText(papszMetadata[i]);
-//        }
+        //        if(CSLCount(papszMetadata)>0)
+        //        {
+        //            for(int i=0;papszMetadata[i]!=NULL;i++)
+        //                ui->plainTextEdit->appendPlainText(papszMetadata[i]);
+        //        }
 
         /* 部分GPS+时间信息 */
         if(CSLCount(papszMetadata)>0)
@@ -103,39 +103,49 @@ void MainWindow::on_pushButton_clicked()
                     QString date = temp1[0];
                     QString time = temp1[1];
 
-                   ui->plainTextEdit->appendPlainText("拍摄日期 : " + date );
-                   ui->plainTextEdit->appendPlainText("拍摄时间 : " + time );
-
+                    ui->plainTextEdit->appendPlainText("拍摄日期 : " + date );
+                    ui->plainTextEdit->appendPlainText("拍摄时间 : " + time );
                 }
-
+            }
         }
-     }
     }
 
     if(str =="GTiff" )
     {
-        QString Projected = "Projection(1:YES;2:NO) :" + QString::number(poDataset->GetSpatialRef()->IsProjected());
-        QString SemiMajor = "长轴半径 = " + QString::number(poDataset->GetSpatialRef()->GetSemiMajor(),'f',0);
-        QString SemiMinor = "短轴半径 = " + QString::number(poDataset->GetSpatialRef()->GetSemiMinor(),'f',9);
-        QString GetInvFlattening = "扁率 = " + QString::number(poDataset->GetSpatialRef()->GetInvFlattening(),'f',9);
-        double test[6];
-        poDataset->GetGeoTransform(test);
-        ui->plainTextEdit->appendPlainText(pic + "TIF");
-        ui->plainTextEdit->appendPlainText(Projected);
-        ui->plainTextEdit->appendPlainText(SemiMajor);
-        ui->plainTextEdit->appendPlainText(SemiMinor);
-        ui->plainTextEdit->appendPlainText(GetInvFlattening);
-        //upleft Coordinate
-        ui->plainTextEdit->appendPlainText("upperCoord = " + QString::number(test[0],'f',4));
-        ui->plainTextEdit->appendPlainText("leftCoord = " + QString::number(test[3],'f',4));
-        //pixel width and height
-        ui->plainTextEdit->appendPlainText("pixel width = " + QString::number(test[1],'f',4));
-        ui->plainTextEdit->appendPlainText("pixel height = " + QString::number(test[5],'f',4));
-
-        //proj信息
-        ui->plainTextEdit->appendPlainText("投影信息:" + QString(poDataset->GetSpatialRef()->GetAttrValue("PROJCS")));
-
-
+        if(poDataset->GetSpatialRef()->IsProjected() == 1)
+        {
+            ui->plainTextEdit->appendPlainText("投影信息：已投影");
+            ui->plainTextEdit->appendPlainText("投影信息:" + QString(poDataset->GetSpatialRef()->GetAttrValue("PROJCS")));
+            GDALRasterBand *poBand = poDataset->GetRasterBand(1);
+            int XSize =  poBand->GetXSize();
+            int YSize = poBand->GetYSize();
+            ui->plainTextEdit->appendPlainText("X尺寸 : " + QString::number(XSize));
+            ui->plainTextEdit->appendPlainText("Y尺寸 : " + QString::number(YSize));
+            double test[6];
+            poDataset->GetGeoTransform(test);
+            //downleft Coordinate
+            double downCoord = test[3] + YSize*test[5];
+//            qreal a = test[3] +YSize*test[5];
+//            float downCoord = qRound64(a);
+            ui->plainTextEdit->appendPlainText("左上坐标 = " + QString::number(test[0],'f',1) + "," +  QString::number(test[3],'f',1));
+            ui->plainTextEdit->appendPlainText("左下坐标 = " + QString::number(test[0],'f',1) + "," +  QString::number(downCoord,'f',1));
+            //pixel width and height
+            ui->plainTextEdit->appendPlainText("像元大小(宽) = " + QString::number(test[1],'f',1));
+            ui->plainTextEdit->appendPlainText("像元大小(高) = " + QString::number(test[5],'f',1));
+        }
+        else
+        {
+            ui->plainTextEdit->appendPlainText("投影信息：未投影");
+            ui->plainTextEdit->appendPlainText("投影信息:" + QString(poDataset->GetSpatialRef()->GetAttrValue("GEOGCS")));
+            double test[6];
+            poDataset->GetGeoTransform(test);
+            //upleft Coordinate
+            ui->plainTextEdit->appendPlainText("上坐标 = " + QString::number(test[0],'f',8));
+            ui->plainTextEdit->appendPlainText("左坐标 = " + QString::number(test[3],'f',8));
+            //pixel width and height
+            ui->plainTextEdit->appendPlainText("像元大小(宽) = " + QString::number(test[1],'f',16));
+            ui->plainTextEdit->appendPlainText("像元大小(高) = " + QString::number(test[5],'f',16));
+        }
         GDALDataset::Bands poBands = poDataset->GetBands();
         for(int i=1;i<poBands.size()+1;i++){
             GDALRasterBand* poBand = poDataset->GetRasterBand(i);
@@ -144,82 +154,14 @@ void MainWindow::on_pushButton_clicked()
         }
 
         //单点查询value值
-        GDALRasterBand *poBand = poDataset->GetRasterBand(1);
-        int nXSize = poBand->GetXSize();
-        float *valueResultList = new float[nXSize];
-        poBand->RasterIO(GF_Read,2010,2000,1,1,valueResultList,nXSize,1,GDT_Float32,0,0);
-        ui->plainTextEdit->appendPlainText(QString::number(valueResultList[0],'f',0));
+        //        GDALRasterBand *poBand = poDataset->GetRasterBand(1);
+        //        int nXSize = poBand->GetXSize();
+        //        float *valueResultList = new float[nXSize];
+        //        poBand->RasterIO(GF_Read,2010,2000,1,1,valueResultList,nXSize,1,GDT_Float32,0,0);
+        //        ui->plainTextEdit->appendPlainText(QString::number(valueResultList[0],'f',0));
 
     };
 
-
-/*
-    //查询坐标系信息
-//    qDebug(poDataset->GetSpatialRef()->GetAttrValue("PROJCS"));
-//    qDebug(poDataset->GetSpatialRef()->GetAttrValue("GEOGCS"));
-//    qDebug(poDataset->GetSpatialRef()->GetAttrValue("DATUM"));
-//    qDebug(poDataset->GetSpatialRef()->GetAttrValue("PRIMEM"));
-//    qDebug(poDataset->GetSpatialRef()->GetAttrValue("UNIT"));
-//    qDebug(poDataset->GetSpatialRef()->GetAttrValue("AUTHORITY"));
-//    qDebug(poDataset->GetSpatialRef()->GetAttrValue("PROJECTION"));
-//    qDebug(poDataset->GetSpatialRef()->GetAttrValue("PARAMETER"));
-//    qDebug(poDataset->GetSpatialRef()->GetAttrValue("AXIS"));
-
-     //查询椭球信息
-//    qDebug("projected :%d",poDataset->GetSpatialRef()->IsProjected());
-//    qDebug("SemiMajor :%f",poDataset->GetSpatialRef()->GetSemiMajor());
-//    qDebug("SemiMinor :%.9f",poDataset->GetSpatialRef()->GetSemiMinor());
-//    qDebug("GetInvFlattening :%.9f",poDataset->GetSpatialRef()->GetInvFlattening());
-
-    //查询偏移信息
-//    qDebug("SRS_PP_CENTRAL_MERIDIAN %f",poDataset->GetSpatialRef()->GetProjParm(SRS_PP_CENTRAL_MERIDIAN,0.0));
-//    qDebug("SRS_PP_STANDARD_PARALLEL_1 %f",poDataset->GetSpatialRef()->GetProjParm(SRS_PP_STANDARD_PARALLEL_1,0.0));
-//    qDebug("SRS_PP_FALSE_EASTING %f",poDataset->GetSpatialRef()->GetProjParm(SRS_PP_FALSE_EASTING,0.0));
-//    qDebug("SRS_PP_FALSE_NORTHING %f",poDataset->GetSpatialRef()->GetProjParm(SRS_PP_FALSE_NORTHING,0.0));
-
-    //
-//    qDebug("%f",poDataset->GetSpatialRef()->GetLinearUnits());
-
-    //查询value范围
-//    GDALRasterBand* poBand = poDataset->GetRasterBand(1);
-//    int bGotMin,bGotMax;
-//    double adfMinMax[2];
-//    adfMinMax[0] = poBand->GetMinimum(&bGotMin);
-//    adfMinMax[1] = poBand->GetMaximum(&bGotMax);
-//    GDALComputeRasterMinMax((GDALRasterBandH)poBand,TRUE,adfMinMax);
-//    qDebug("Min=%.3f,Max=%.3f",adfMinMax[0],adfMinMax[1]);
-
-
-//    QString Projected = "Projection(1:YES;2:NO) :" + QString::number(poDataset->GetSpatialRef()->IsProjected());
-//    QString SemiMajor = "长轴半径 = " + QString::number(poDataset->GetSpatialRef()->GetSemiMajor(),'f',0);
-//    QString SemiMinor = "短轴半径 = " + QString::number(poDataset->GetSpatialRef()->GetSemiMinor(),'f',9);
-//    QString GetInvFlattening = "扁率 = " + QString::number(poDataset->GetSpatialRef()->GetInvFlattening(),'f',9);
-//    double test[6];
-//    poDataset->GetGeoTransform(test);
-//    ui->plainTextEdit->clear();
-//    ui->plainTextEdit->appendPlainText(Projected);
-//    ui->plainTextEdit->appendPlainText(SemiMajor);
-//    ui->plainTextEdit->appendPlainText(SemiMinor);
-//    ui->plainTextEdit->appendPlainText(GetInvFlattening);
-//    //upleft Coordinate
-//    ui->plainTextEdit->appendPlainText("upperCoord = " + QString::number(test[0],'f',4));
-//    ui->plainTextEdit->appendPlainText("leftCoord = " + QString::number(test[3],'f',4));
-//    //pixel width and height
-//    ui->plainTextEdit->appendPlainText("pixel width = " + QString::number(test[1],'f',4));
-//    ui->plainTextEdit->appendPlainText("pixel height = " + QString::number(test[5],'f',4));
-//    //变形信息
-////    ui->plainTextEdit->appendPlainText(QString::number(test[2],'f',4));
-////    ui->plainTextEdit->appendPlainText(QString::number(test[4],'f',4));
-
-//    //查询value范围
-//    GDALDataset::Bands poBands = poDataset->GetBands();
-//    for(int i=1;i<poBands.size()+1;i++){
-//        GDALRasterBand* poBand = poDataset->GetRasterBand(i);
-//        ui->plainTextEdit->appendPlainText("波段" + QString::number(i) + "最小值 : " + QString::number(getBandInfoMin(poBand).first()));
-//        ui->plainTextEdit->appendPlainText("波段" + QString::number(i) + "最大值 : " + QString::number(getBandInfoMin(poBand).last()));
-//    }
-
-*/
 }
 
 
--- mainwindow.h
+++ mainwindow.h
@@ -26,3 +26,96 @@ private:
     Ui::MainWindow *ui;
 };
 #endif // MAINWINDOW_H
+
+
+//        QString Projected = "Projection(1:YES;2:NO) :" + QString::number(poDataset->GetSpatialRef()->IsProjected());
+//        QString SemiMajor = "长轴半径 = " + QString::number(poDataset->GetSpatialRef()->GetSemiMajor(),'f',0);
+//        QString SemiMinor = "短轴半径 = " + QString::number(poDataset->GetSpatialRef()->GetSemiMinor(),'f',9);
+//        QString GetInvFlattening = "扁率 = " + QString::number(poDataset->GetSpatialRef()->GetInvFlattening(),'f',9);
+//        double test[6];
+//        poDataset->GetGeoTransform(test);
+//        ui->plainTextEdit->appendPlainText(pic + "TIF");
+//        ui->plainTextEdit->appendPlainText(Projected);
+//        ui->plainTextEdit->appendPlainText(SemiMajor);
+//        ui->plainTextEdit->appendPlainText(SemiMinor);
+//        ui->plainTextEdit->appendPlainText(GetInvFlattening);
+        //upleft Coordinate
+//        ui->plainTextEdit->appendPlainText("upperCoord = " + QString::number(test[0],'f',4));
+//        ui->plainTextEdit->appendPlainText("leftCoord = " + QString::number(test[3],'f',4));
+        //pixel width and height
+//        ui->plainTextEdit->appendPlainText("pixel width = " + QString::number(test[1],'f',4));
+//        ui->plainTextEdit->appendPlainText("pixel height = " + QString::number(test[5],'f',4));
+
+        //proj信息
+//        ui->plainTextEdit->appendPlainText("投影信息:" + QString(poDataset->GetSpatialRef()->GetAttrValue("PROJCS")));
+//        ui->plainTextEdit->appendPlainText("投影信息:" + QString(poDataset->GetSpatialRef()->GetAttrValue("GEOGCS")));
+//        ui->plainTextEdit->appendPlainText("投影信息:" + QString(poDataset->GetSpatialRef()->GetAttrValue("DATUM")));
+//        ui->plainTextEdit->appendPlainText("投影信息:" + QString(poDataset->GetSpatialRef()->GetAttrValue("PRIMEM")));
+//        ui->plainTextEdit->appendPlainText("投影信息:" + QString(poDataset->GetSpatialRef()->GetAttrValue("PROJECTION")));
+/*
+    //查询坐标系信息
+//    qDebug(poDataset->GetSpatialRef()->GetAttrValue("PROJCS"));
+//    qDebug(poDataset->GetSpatialRef()->GetAttrValue("GEOGCS"));
+//    qDebug(poDataset->GetSpatialRef()->GetAttrValue("DATUM"));
+//    qDebug(poDataset->GetSpatialRef()->GetAttrValue("PRIMEM"));
+//    qDebug(poDataset->GetSpatialRef()->GetAttrValue("UNIT"));
+//    qDebug(poDataset->GetSpatialRef()->GetAttrValue("AUTHORITY"));
+//    qDebug(poDataset->GetSpatialRef()->GetAttrValue("PROJECTION"));
+//    qDebug(poDataset->GetSpatialRef()->GetAttrValue("PARAMETER"));
+//    qDebug(poDataset->GetSpatialRef()->GetAttrValue("AXIS"));
+
+     //查询椭球信息
+//    qDebug("projected :%d",poDataset->GetSpatialRef()->IsProjected());
+//    qDebug("SemiMajor :%f",poDataset->GetSpatialRef()->GetSemiMajor());
+//    qDebug("SemiMinor :%.9f",poDataset->GetSpatialRef()->GetSemiMinor());
+//    qDebug("GetInvFlattening :%.9f",poDataset->GetSpatialRef()->GetInvFlattening());
+
+    //查询偏移信息
+//    qDebug("SRS_PP_CENTRAL_MERIDIAN %f",poDataset->GetSpatialRef()->GetProjParm(SRS_PP_CENTRAL_MERIDIAN,0.0));
+//    qDebug("SRS_PP_STANDARD_PARALLEL_1 %f",poDataset->GetSpatialRef()->GetProjParm(SRS_PP_STANDARD_PARALLEL_1,0.0));
+//    qDebug("SRS_PP_FALSE_EASTING %f",poDataset->GetSpatialRef()->GetProjParm(SRS_PP_FALSE_EASTING,0.0));
+//    qDebug("SRS_PP_FALSE_NORTHING %f",poDataset->GetSpatialRef()->GetProjParm(SRS_PP_FALSE_NORTHING,0.0));
+
+    //
+//    qDebug("%f",poDataset->GetSpatialRef()->GetLinearUnits());
+
+    //查询value范围
+//    GDALRasterBand* poBand = poDataset->GetRasterBand(1);
+//    int bGotMin,bGotMax;
+//    double adfMinMax[2];
+//    adfMinMax[0] = poBand->GetMinimum(&bGotMin);
+//    adfMinMax[1] = poBand->GetMaximum(&bGotMax);
+//    GDALComputeRasterMinMax((GDALRasterBandH)poBand,TRUE,adfMinMax);
+//    qDebug("Min=%.3f,Max=%.3f",adfMinMax[0],adfMinMax[1]);
+
+
+//    QString Projected = "Projection(1:YES;2:NO) :" + QString::number(poDataset->GetSpatialRef()->IsProjected());
+//    QString SemiMajor = "长轴半径 = " + QString::number(poDataset->GetSpatialRef()->GetSemiMajor(),'f',0);
+//    QString SemiMinor = "短轴半径 = " + QString::number(poDataset->GetSpatialRef()->GetSemiMinor(),'f',9);
+//    QString GetInvFlattening = "扁率 = " + QString::number(poDataset->GetSpatialRef()->GetInvFlattening(),'f',9);
+//    double test[6];
+//    poDataset->GetGeoTransform(test);
+//    ui->plainTextEdit->clear();
+//    ui->plainTextEdit->appendPlainText(Projected);
+//    ui->plainTextEdit->appendPlainText(SemiMajor);
+//    ui->plainTextEdit->appendPlainText(SemiMinor);
+//    ui->plainTextEdit->appendPlainText(GetInvFlattening);
+//    //upleft Coordinate
+//    ui->plainTextEdit->appendPlainText("upperCoord = " + QString::number(test[0],'f',4));
+//    ui->plainTextEdit->appendPlainText("leftCoord = " + QString::number(test[3],'f',4));
+//    //pixel width and height
+//    ui->plainTextEdit->appendPlainText("pixel width = " + QString::number(test[1],'f',4));
+//    ui->plainTextEdit->appendPlainText("pixel height = " + QString::number(test[5],'f',4));
+//    //变形信息
+////    ui->plainTextEdit->appendPlainText(QString::number(test[2],'f',4));
+////    ui->plainTextEdit->appendPlainText(QString::number(test[4],'f',4));
+
+//    //查询value范围
+//    GDALDataset::Bands poBands = poDataset->GetBands();
+//    for(int i=1;i<poBands.size()+1;i++){
+//        GDALRasterBand* poBand = poDataset->GetRasterBand(i);
+//        ui->plainTextEdit->appendPlainText("波段" + QString::number(i) + "最小值 : " + QString::number(getBandInfoMin(poBand).first()));
+//        ui->plainTextEdit->appendPlainText("波段" + QString::number(i) + "最大值 : " + QString::number(getBandInfoMin(poBand).last()));
+//    }
+
+*/
